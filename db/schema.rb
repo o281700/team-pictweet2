53c9b0dbe49afb3ec569ce75eeebddeaaebb9a3c
kiserukun <73691576+kiserukun@users.noreply.github.com>
2020-12-16 15:18:49 +0900

    koreya

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_12_16_032807) do
     t.index ["tweet_id"], name: "index_comments_on_tweet_id"
   end
 
-  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "text"
     t.text "image"
